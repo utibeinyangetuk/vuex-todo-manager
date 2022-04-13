ab7b560b9afc3f2090bcc874e5c6e2e3ff814007
utibe inyangetuk <inyangutibz@gmail.com>
2022-04-13 11:34:49 +0100

    created a Todos component and added some static data to the store index.js file to test it

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<div class="container">
+			<Todos />
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import Todos from "@/components/Todos";
+	export default {
+		name: "App",
+		components: {
+			Todos,
+		},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	body {
+		font-family: "franklin gothic medium", "arial narrow", Arial, Helvetica,
+			sans-serif;
+		line-height: 1.6;
+		background: #e8f7f0;
+	}
+	.container {
+		max-width: 1100px;
+		margin: auto;
+		overflow: hidden;
+		padding: 0 2rem;
+	}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		todos: [
+			{
+				id: 1,
+				title: "Todo one",
+			},
+			{
+				id: 2,
+				title: "Todo two",
+			},
+			{
+				id: 3,
+				title: "Todo three",
+			},
+		],
+	},
+	getters: {
+		allTodos: (state) => {
+			return state.todos;
+		},
+	},
+	mutations: {},
+	actions: {},
+	modules: {},
+});
